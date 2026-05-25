7c0e1c20287d9a26cf1d907af136cc033c8eda11
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2026-05-25 11:46:54 -0300

    Estrutura Lista-Alunos

---
+++ src/components/Tabela/index.tsx
@@ -0,0 +1,28 @@
+
+import Input from "../Input"
+
+export default Tabela(){
+    return (
+
+        <Container>
+            <Header>
+                <TitleContainer>
+                    <Espaco> Lista de Alunos </Espaco>
+                </TitleContainer>
+            </Header>
+            <Row>
+                <Input
+                    size="medium"
+                    type="text"
+                    placeholder="Pesquisar Aluno"
+                />
+
+                
+
+                <TableContainer>
+
+                </>
+                <Container />
+
+                )
+}
